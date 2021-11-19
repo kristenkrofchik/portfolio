d872693600d0a7493533a8848234e996c84c52c8
Kristen Krofchik <k.krofchik@gmail.com>
2021-11-18 19:28:11 -0600

    working on page skeleton

--- portfolio-page/src/App.js
+++ portfolio-page/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/Homepage';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Connect from './components/Connect';
+import Projects from './components/Projects';
 
 function App() {
   return (
     <div className="App">
-      <h1>Kristen Krofchik</h1>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/about' component={About} />
+        <Route path='/projects' component={Projects} />
+        <Route path='/connect' component={Connect} />
+        <Route path='*' component={NotFound} />
+      </Switch>
     </div>
   );
 }
