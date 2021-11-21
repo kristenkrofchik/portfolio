54ea53a4f06935844f04575f5d03449706543b55
Kristen Krofchik <k.krofchik@gmail.com>
2021-11-21 11:58:02 -0600

    home looking pretty good.

--- portfolio-page/src/components/Hero.js
+++ portfolio-page/src/components/Hero.js
@@ -3,15 +3,16 @@ import { withRouter } from 'react-router-dom';
 import './Hero.css';
 
 const Hero = ({ history }) => {
+    let headline = `Hello! I am Kristen Krofchik \n A Full Stack Software Developer`;
     return (
         <section className='hero'>
             <div className='hero-body'>
                 <h1 className='hero-title'>
-                Hello! I am Kristen Krofchik, a full stack software developer.
+                {headline}
                 </h1>
                 <div className='learn-more-btn'>
                     <button onClick={()=> history.push('/about')}>
-                        LEARN MORE 
+                        Learn More 
                     </button>
                 </div>
             </div>
