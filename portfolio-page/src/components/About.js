cc9f43e7f19eb24bd1e29ff3a3a1d6b826d58288
Kristen Krofchik <k.krofchik@gmail.com>
2021-11-29 10:47:41 -0600

    about and hero changes

--- portfolio-page/src/components/About.js
+++ portfolio-page/src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../shared/Layout';
+import profile_photo from '../images/profile_photo.JPG'
 import './About.css';
 
 const About = () => {
@@ -14,9 +15,11 @@ const About = () => {
                         <div class="content">
                             <p>
                             I am a junior software developer eager to learn and grow in the technology industry. I have spent years learning to code in my personal time, and have now completed Springboard’s Software Engineering bootcamp to make software development my professional career. I have experience building websites and web applications with JavaScript, CSS, HTML, Python, Flask, SQL, Node.js, and ReactJS.
-                            
+                            </p>
+                            <p>
                             I am currently the co-founder and operator of a thriving online vintage and antiques resale business. Before that, I spent several years in the vibrant fine dining restaurant industry in Chicago, IL, where I learned to think on my feet, solve problems quickly, and work with a varied team. I am thrilled to work in fields marked by constant learning and new experiences and am happy I can continue to do so as a software developer.
-
+                            </p>
+                            <p>
                             I graduated magna cum laude from Northwestern University with a BA in history. I chose to study history because I enjoyed discovering, analyzing, and communicating the various forces that shaped historical events. The analytical skills I gained as a student have served me well as a software developer.
                             </p>
                         </div>
@@ -26,7 +29,7 @@ const About = () => {
                         </div>
                     </div>
                     <div class="image-section">
-                        <img src="../images/profile_photo.jpg" alt="Kristen Krofchik, website owner" />
+                        <img src={profile_photo} alt="Kristen Krofchik, website owner" />
                     </div>
                 </div>
             </div>
