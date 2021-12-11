6cd5cc8ab32af93e36ef0b4db669d93e1596dc07
Kristen Krofchik <k.krofchik@gmail.com>
2021-12-10 20:57:59 -0600

    playing around with design

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Box, Grommet } from 'grommet';
+
+const theme = {
+  global: {
+    colors: {
+      
+    }
+    font: {
+      family: 'DM Sans',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box 
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='brand'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
+
+function App() {
+  return (
+    <Grommet theme={theme}>
+      <AppBar>Hello</AppBar>
+    </Grommet>
+  );
+}
+
+export default App;
