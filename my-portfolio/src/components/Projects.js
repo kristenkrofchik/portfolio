dbefc7bc6fc32b58024bb00f5cdd035209b2b0e2
Kristen Krofchik <k.krofchik@gmail.com>
2021-12-20 08:01:14 -0600

    starting projects. need to make list of objects for projects next

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavBar from '../shared/NavBar';
 import SiteFooter from '../shared/SiteFooter';
+import { Heading, Main} from 'grommet';
 
 const Projects = () => {
     return (
         <>
             <NavBar />
+            <Main pad="medium">
+                <Heading level="1" responsive="true" textAlign="center">Projects</Heading>
+            </Main>
             <SiteFooter />
         </>
     )
