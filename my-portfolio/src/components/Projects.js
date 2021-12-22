a23e18795248daf1be93f713044b0decc20c1eb7
Kristen Krofchik <k.krofchik@gmail.com>
2021-12-22 13:46:13 -0600

    projects page has info needs styling

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../shared/NavBar';
 import SiteFooter from '../shared/SiteFooter';
+import ProjectCardList from './ProjectCardList';
 import { Heading, Main} from 'grommet';
 
 const Projects = () => {
@@ -9,6 +10,7 @@ const Projects = () => {
             <NavBar />
             <Main pad="medium">
                 <Heading level="1" responsive="true" textAlign="center">Projects</Heading>
+                <ProjectCardList />
             </Main>
             <SiteFooter />
         </>
