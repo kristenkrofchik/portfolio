15f2f14594857f1de3e9d12a51aa94322243aa5f
Kristen Krofchik <k.krofchik@gmail.com>
2021-12-27 18:39:25 -0600

    working on about me text

---
+++ my-portfolio/src/components/AboutText.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Paragraph } from 'grommet';
+
+const AboutText = () => {
+    <Box align="left" justify="center" pad="small" gap="medium" color="light-5">
+        <Paragraph margin="xsmall">
+            I am a software developer eager to learn and grow in the technology industry. I have spent years learning to code in my personal time, and have now completed the Springboard Software Engineering bootcamp to make software development my professional career. I have experience building websites and web applications with JavaScript, React, Node.js, Python, Flask, SQL, and more.
+        </Paragraph>
+        <Paragraph margin="xsmall">
+            I am the co-founder and operator of a thriving online vintage and antiques resale business, Pirate Chicks Vintage. Before that, I spent several years in the vibrant fine dining restaurant industry in Chicago, IL, where I learned to think on my feet, solve problems quickly, and work with a varied team. I am thrilled to work in fields marked by constant learning and new experiences and am happy I can continue to do so as a software developer.
+        </Paragraph>
+        <Paragraph margin="xsmall">
+            I graduated magna cum laude and Phi Beta Kappa from Northwestern University with a BA in history. I chose to study history because I enjoyed discovering, analyzing, and communicating the various forces that shaped historical events. The analytical skills I gained as a student have served me well as a software developer.
+        </Paragraph>
+    </Box>
+};
+
+export default AboutText; 
