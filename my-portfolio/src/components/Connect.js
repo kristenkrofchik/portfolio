6d19ce9e44bee9ced533e076d279f278866a40b8
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-08 00:27:13 -0600

    getting skeleton for connect need to do functions

--- my-portfolio/src/components/Connect.js
+++ my-portfolio/src/components/Connect.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import NavBar from '../shared/NavBar';
 import SiteFooter from '../shared/SiteFooter';
-import { Box, Button, Form, FormField, Heading, TextArea, TextInput } from 'grommet';
+import { Box, Button, Form, FormField, Heading, MaskedInput, TextArea, TextInput } from 'grommet';
 
 const defaultValue = {
     name: '',
@@ -10,10 +10,37 @@ const defaultValue = {
 };
 
 const Connect = () => {
+    const [value, setValue] = useState(defaultValue);
+
     return (
         <>
             <NavBar />
             <Heading level="2" responsive="true" textAlign="left" className="heading">Connect</Heading>
+            <Box fill align="center" justify="center">
+                <Box width="medium">
+                    <Form value={value} onChange={} onSubmit={}>
+                        <FormField label="Name" name="name">
+                            <TextInput name="name" />
+                        </FormField>
+                        <FormField label="Email" name="email">
+                            <MaskedInput name="email"
+                            mask={[
+                                    { regexp: /^[\w\-_.]+$/, placeholder: 'example' },
+                                    { fixed: '@' },
+                                    { regexp: /^[\w]+$/, placeholder: 'my' },
+                                    { fixed: '.' },
+                                    { regexp: /^[\w]+$/, placeholder: 'com' },
+                                ]} />
+                        </FormField>
+                        <FormField label="Message" name="message">
+                            <TextArea name="message" />
+                        </FormField>
+                        <Box direction="row" justify="between" margin={{ top: 'medium' }}>
+                            <Button type="submit" label="Send" />
+                        </Box>
+                    </Form>
+                </Box>
+            </Box>
             <SiteFooter />
         </>
     )
