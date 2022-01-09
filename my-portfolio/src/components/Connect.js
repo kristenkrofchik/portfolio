93e0ce59b8a18648fdc158ddcdafb2333817f04e
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-08 23:04:40 -0600

    working on connect. getting code right then need to send email

--- my-portfolio/src/components/Connect.js
+++ my-portfolio/src/components/Connect.js
@@ -12,18 +12,32 @@ const defaultValue = {
 const Connect = () => {
     const [value, setValue] = useState(defaultValue);
 
+    const handleSubmit = evt => {
+        evt.preventDefault();
+
+        setValue(defaultValue);
+    };
+
+    const handleChange = evt => {
+        const { valueName, value } = evt.target;
+        setValue(data => ({
+            ...data,
+            [valueName]: value
+        }));
+    };
+
     return (
         <>
             <NavBar />
             <Heading level="2" responsive="true" textAlign="left" className="heading">Connect</Heading>
             <Box fill align="center" justify="center">
                 <Box width="medium">
-                    <Form value={value} onChange={} onSubmit={}>
+                    <Form value={value} onSubmit={handleSubmit}>
                         <FormField label="Name" name="name">
-                            <TextInput name="name" />
+                            <TextInput name="name" onChange={handleChange}/>
                         </FormField>
                         <FormField label="Email" name="email">
-                            <MaskedInput name="email"
+                            <MaskedInput name="email" onChange={handleChange} 
                             mask={[
                                     { regexp: /^[\w\-_.]+$/, placeholder: 'example' },
                                     { fixed: '@' },
@@ -33,7 +47,7 @@ const Connect = () => {
                                 ]} />
                         </FormField>
                         <FormField label="Message" name="message">
-                            <TextArea name="message" />
+                            <TextArea name="message" onChange={handleChange} />
                         </FormField>
                         <Box direction="row" justify="between" margin={{ top: 'medium' }}>
                             <Button type="submit" label="Send" />
