ca9e8ed2fd8f9aa8f0c3e03d6a285add63b2bff9
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-15 15:15:16 -0600

    working on better layout for mobile size

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -9,10 +9,10 @@ const About = () => {
         <>
             <NavBar />
             <Heading level="2" responsive="true" textAlign="left" className="heading" color="neutral-1">About Me</Heading>
-            <Grid columns={{ count: 2, size: 'auto' }} gap='medium'>
+            <Grid columns={{ count: 2, size: 'auto' }} gap='medium' responsive="true">
                 <Box>
                     <Paragraph margin={{ top:'large', left: 'large' }} color="neutral-1">
-                    I am a resourceful and meticulous full stack software developer passionate about using my technical skills to build a better world. After studying development on my own for several years, I recently completed a rigorous 1+ year software engineering coding bootcamp with Springboard, where I designed, built, tested and launched full stack web applications and APIs with JavaScript, Node.js, React, Python, Flask, PSQL and more. 
+                    I am a full stack software developer passionate about using my technical skills to build a better world. After studying development on my own for several years, I recently completed a rigorous 1+ year software engineering coding bootcamp with Springboard, where I designed, built, tested and launched full stack web applications and APIs with JavaScript, Node.js, React, Python, Flask, PSQL and more. 
                     </Paragraph>
                     <Paragraph margin={{ top:'small', left: 'large' }} color="neutral-1">
                     When I am not coding, I run a vintage e-commerce business with my mother, where I have learned how to balance several roles to create a respected brand and a high-quality experience for my customers. Before that, I worked in the restaurant industry for over a decade. In hospitality, clear communication and constant collaboration are everything! I use these invaluable skills to meaningfully contribute to a team from the start. 
