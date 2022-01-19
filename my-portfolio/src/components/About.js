11ba0ea2c374a1bacdf43f88f9052ce607790fc3
Kristen Krofchik <k.krofchik@gmail.com>
2022-01-18 18:30:44 -0600

    pic finally centered-- box in box

--- my-portfolio/src/components/About.js
+++ my-portfolio/src/components/About.js
@@ -11,8 +11,10 @@ const About = () => {
                 <NavBar />
                 <Heading level="2" responsive="true" textAlign="left" className="heading" color="neutral-1">About Me</Heading>
                 <Grid rows={{ count: 2, size: 'auto' }} gap='medium' responsive="true"> 
-                    <Box height="medium" width="medium" align="center">
-                        <Image className="profile-image" fit="contain" opacity="strong" src={profile_photo} margin={{ top:'large', left: 'medium', right: 'large', bottom:'medium' }}/>
+                    <Box align="center" justify="center">
+                        <Box height="medium" width="medium" align="center">
+                            <Image className="profile-image" fit="cover" opacity="strong" src={profile_photo} />
+                        </Box>
                     </Box>
                     <Box justify="center" align="center">
                         <Paragraph margin={{ top:'medium' }} color="neutral-1">
@@ -21,7 +23,7 @@ const About = () => {
                         <Paragraph margin={{ top:'small' }} color="neutral-1">
                         When I am not coding, I run a vintage e-commerce business with my mother, where I have learned how to balance several roles to create a respected brand and a high-quality experience for my customers. Before that, I worked in the restaurant industry for over a decade. In hospitality, clear communication and constant collaboration are everything! I use these invaluable skills to meaningfully contribute to a team from the start. 
                         </Paragraph>
-                        <Paragraph margin={{ top:'small' }} color="neutral-1">
+                        <Paragraph margin={{ top:'small', bottom:'large' }} color="neutral-1">
                         I would love to work with you! View my resume <a href="https://drive.google.com/file/d/1kJPcjDxJTT5eKYt4f7RecqjvBZQQt3pS/view?usp=sharing">here</a>
                         </Paragraph>
                     </Box>
